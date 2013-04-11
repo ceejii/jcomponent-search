b2176f0361a350b826b2bda05cce6d9fadc5893c
ceejii <github.com@cj.se>
2013-04-11 09:20:54 +0200

    Added a search delay in the example SearchProvider implementation. Also put the searching in a separate thread so it doesn't freeze the Gui.

--- src/com/ceejii/search/ExampleSearchProvider.java
+++ src/com/ceejii/search/ExampleSearchProvider.java
@@ -2,6 +2,8 @@ package com.ceejii.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 
@@ -11,28 +13,70 @@ import com.ceejii.gui.component.SearchSuggestionsDisplayer;
 
 public class ExampleSearchProvider implements SearchProvider {
 
+	private String searchString;
+	private SearchSuggestionsDisplayer view;
+	private int resultCount;
+	private List<String> db;
+
 	public void quickSearchForString(String searchString,
 			SearchSuggestionsDisplayer view, int resultCount) {
-		//NOTE: For future Java versions the view could instead be a function pointer/lambda that handles the display of the result.
-		List<String> db = new ArrayList<String>();
-		db.add("Stockholm");
-		db.add("Göteborg");
-		db.add("Malmö");
-		db.add("Stockport");
-		List<String> results = new ArrayList<String>();
-		String lowerCase = searchString.toLowerCase();
-		if (!searchString.equals("")) {
-			for (String line : db) {
-				if (line.toLowerCase().startsWith(lowerCase)) {
-					results.add(line);
-				}
-			}
+		int searchDelayMilliSec = 400;
+		synchronized (new Object()) {
+			this.searchString = searchString;
 		}
-		if(results.size() > resultCount) {
-			results = results.subList(0, resultCount);
-		}
-		view.showSearchResults(results, new ExampleSearchSuggestionListener());
+		this.view = view;
+		this.resultCount = resultCount;
+		delayedSearch(searchDelayMilliSec);
 
+	}
+
+	private void search(String searchString) {
+		synchronized (new Object()) {
+			db = new ArrayList<String>();
+			db.add("Stockholm");
+			db.add("Göteborg");
+			db.add("Malmö");
+			db.add("Stockport");
+		}
+		Thread thread = new Thread() {
+
+			@Override
+			public void run() {
+				List<String> results = new ArrayList<String>();
+				String searchString = "";
+				synchronized (new Object()) {
+					searchString = ExampleSearchProvider.this.searchString;
+				}
+				String lowerCase = searchString.toLowerCase();
+				if (!searchString.equals("")) {
+					for (String line : db) {
+						if (line.toLowerCase().startsWith(lowerCase)) {
+							results.add(line);
+						}
+					}
+				}
+				if(results.size() > resultCount) {
+					results = results.subList(0, resultCount);
+				}
+				view.showSearchResults(results, new ExampleSearchSuggestionListener());
+			}
+		};
+		thread.start();
+	}
+
+	private boolean delayedSearch(int searchDelayMilliSec) {
+		Timer timer = new Timer();
+		final String originalSearchString = this.searchString;
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				synchronized (new Object()) {
+					if(originalSearchString.equals(ExampleSearchProvider.this.searchString)) {
+							ExampleSearchProvider.this.search(originalSearchString);
+					}
+				}
+			}},	 searchDelayMilliSec);
+		return true;
 	}
 
 	public void fullSearchForString(String searchString,
